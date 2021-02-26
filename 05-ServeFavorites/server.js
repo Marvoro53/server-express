d473a88f6394fb948b726454e1fd3a4e406f4c1e
Marvoro53 <marvinlara321@gmail.com>
2021-02-25 21:32:10 -0600

    created server

--- 05-ServeFavorites/server.js
+++ 05-ServeFavorites/server.js
@@ -1,11 +1,11 @@
-// // Require dependencies
-// var http = require("http");
-// var fs = require("fs");
+// Require dependencies
+var http = require("http");
+var fs = require("fs");
 
-// // Set our port to 8080
-// var PORT = 8080;
+// Set our port to 8080
+var PORT = 8080;
 
-// var server = http.createServer(handleRequest);
+var server = http.createServer(handleRequest);
 
 // function handleRequest(req, res) {
 
