f592d677648801c00767babc9763c7131c68be94
Marvoro53 <marvinlara321@gmail.com>
2021-02-25 21:33:15 -0600

    added handle and switch

--- 05-ServeFavorites/server.js
+++ 05-ServeFavorites/server.js
@@ -7,23 +7,23 @@ var PORT = 8080;
 
 var server = http.createServer(handleRequest);
 
-// function handleRequest(req, res) {
+function handleRequest(req, res) {
 
-//   // Capture the url the request is made to
-//   var path = req.url;
+  // Capture the url the request is made to
+  var path = req.url;
 
-//   // When we visit different urls, read and respond with different files
-//   switch (path) {
-//     case "/food":
-//     case "/movies":
-//     case "/frameworks":
-//       return renderHTML(path, res);
+  // When we visit different urls, read and respond with different files
+  switch (path) {
+    case "/food":
+    case "/movies":
+    case "/frameworks":
+      return renderHTML(path, res);
 
-//     // default to rendering index.html, if none of above cases are hit
-//     default:
-//       return renderHTML('/index.html', res);
-//   }
-// }
+    // default to rendering index.html, if none of above cases are hit
+    default:
+      return renderHTML('/index.html', res);
+  }
+}
 
 // function renderHTML(filePath, res){
 //  return fs.readFile(__dirname + filePath, function(err, data) {
