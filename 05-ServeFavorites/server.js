aa51a3a9c0304a4daa3d4e14f4c88443a258dde8
Marvoro53 <marvinlara321@gmail.com>
2021-03-02 13:29:08 -0600

    aupdated

--- 05-ServeFavorites/server.js
+++ 05-ServeFavorites/server.js
@@ -9,19 +9,18 @@ var server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
 
-  // Capture the url the request is made to
   var path = req.url;
 
   // When we visit different urls, read and respond with different files
   switch (path) {
-    case "/food":
-    case "/movies":
-    case "/frameworks":
+    case "/food.html":
+    case "/movies.html":
+    case "/frameworks.html":
       return renderHTML(path, res);
 
-    // default to rendering index.html, if none of above cases are hit
-    default:
-      return renderHTML('/index.html', res);
+  // default to rendering index.html, if none of above cases are hit
+  default:
+    return renderHTML('/index.html', res);
   }
 }
 
@@ -33,5 +32,5 @@ function renderHTML(filePath, res){
 }
 // Starts our server.
 server.listen(PORT, function() {
-  console.log("Server is listening on PORT: " + PORT);
+  console.log("Server is listening on https://localhost:" + PORT);
 });
